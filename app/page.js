7da2ae9ab493c56a3fb48abfebb4e8d589ef47d1
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-10-17 13:53:18 +0300

    Add bottom margin

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
  
   return (
     <main className='flex justify-center homecontainer rounded-2xl' >
-      <div className='flex flex-col gap-8 md:flex-row w-full md:flex'>
+      <div className='flex flex-col gap-8 mb-5 md:flex-row w-full md:flex'>
         <img className= 'rounded-r-full rounded-b-full max-h-96 max-w-md md:w-1/3'  src='/profile.jpg'/>
         <div className='flex shrink flex-col space-y-8 mt-5'>
         <h1 className='text-5xl font-bold'> Yashodhi Chathurangi</h1>
