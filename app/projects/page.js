7c82627a2ea938ce7304b4b4eac86fcdbfcc6dd9
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-10-20 17:37:14 +0300

    add projects mock data

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./projects.css";
+import { tsParticles } from "tsparticles-engine";
 const Projects = () => {
     const projects = [
         { 
@@ -15,21 +17,31 @@ const Projects = () => {
         image: './house.png',
         name: 'RentHub', 
         tech: ['ROR', 'React', 'Redux']
+        },
+        { 
+            image: './house.png',
+            name: 'RentHub', 
+            tech: ['ROR', 'React', 'Redux']
+        },
+        { 
+            image: './house.png',
+            name: 'RentHub', 
+            tech: ['ROR', 'React', 'Redux']
         }
     ]
+
     return (
     <>
-        <h1>Projects</h1>
-        <div className='flex justify-center h-96 w-full'>
-            <div className="grid items-center grid-cols-3 gap-4 mt-6 mx-6 snap-x snap-mandatory  bg-slate-200 h-80 w-full">
+        <h1 className=' text-2xl'>Projects</h1>   
+        <div className='flex justify-center h-96 w-88 '>
+            <div className="grid grid-flow-col gap-4 mt-6 mx-6 snap-x snap-mandatory h-88 w-full overflow-x-scroll scroll-smooth scrollbar-hide">
             {projects.map((p,index) => (
-            <div key={index} className="flex mx-2 bg-red-700 h-72 w-80 text-white">
-              <h2>{p.name}</h2>
+            <div key={index} className="ring-4 rounded-xl border-2 border-white flex mx-4 my-4 h-72 w-72  text-white hover:scale-110 snap-normal snap-center ">
+              <h2 className=' text-lg text-center w-full'>{p.name}</h2>
             </div>
             ))}
             </div>
-        </div>
-        
+         </div>   
     </>
     )
         
